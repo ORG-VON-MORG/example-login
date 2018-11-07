efd10bed58d65cc1335b3afcd99be8552ab0289a
TG <orggottschalk@web.de>
2018-11-07 23:19:44 +0100

    Initial commit

---
+++ src/main/java/com/example/examplelogin/ExampleLoginApplication.java
@@ -0,0 +1,12 @@
+package com.example.examplelogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleLoginApplication.class, args);
+	}
+}
---
+++ src/test/java/com/example/examplelogin/ExampleLoginApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.examplelogin;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ExampleLoginApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
