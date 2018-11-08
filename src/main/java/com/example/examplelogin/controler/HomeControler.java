bf13e2769334896de842b951d0ccb974d254e88a
master <orggottschalk@web.de>
2018-11-08 23:44:31 +0100

    Inital Commit

--- src/main/java/com/example/examplelogin/controler/HomeControler.java
+++ src/main/java/com/example/examplelogin/controler/HomeControler.java
@@ -1,27 +1,44 @@
 package com.example.examplelogin.controler;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class HomeControler {
 
     @GetMapping("/")
-    public String root() {
-        return "index";
+    public String home1() {
+        return "/home";
+    }
+
+    @GetMapping("/home")
+    public String home() {
+        return "/home";
+    }
+
+    @GetMapping("/admin")
+    public String admin() {
+        return "/admin";
     }
 
     @GetMapping("/user")
-    public String userIndex() {
-        return "user/index";
+    public String user() {
+        return "/user";
+    }
+
+    @GetMapping("/about")
+    public String about() {
+        return "/about";
     }
 
     @GetMapping("/login")
     public String login() {
-        return "login";
+        return "/login";
     }
 
-    @GetMapping("/access-denied")
-    public String accessDenied() {
-        return "/error/access-denied";
+    @GetMapping("/403")
+    public String error403() {
+        return "/error/403";
     }
 
 }
