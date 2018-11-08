5c9e403349de38339c68b485761bab78a8a13e68
master <orggottschalk@web.de>
2018-11-08 23:35:21 +0100

    Inital Commit

---
+++ src/main/java/com/example/examplelogin/controler/HomeControler.java
@@ -0,0 +1,27 @@
+package com.example.examplelogin.controler;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+public class HomeControler {
+
+    @GetMapping("/")
+    public String root() {
+        return "index";
+    }
+
+    @GetMapping("/user")
+    public String userIndex() {
+        return "user/index";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/access-denied")
+    public String accessDenied() {
+        return "/error/access-denied";
+    }
+
+}
